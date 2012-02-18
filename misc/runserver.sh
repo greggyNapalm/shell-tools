24f1dea5b17b3b947aaa959e46084e2d48a3138f
Gregory Komissarov <gregory.komissarov@gmail.com>
2012-02-18 14:32:23 +0400

    GAE dev inst init updated, gunicorn syntx fix

--- misc/runserver.sh
+++ misc/runserver.sh
@@ -7,7 +7,7 @@ args=$args'--datastore_path=/Users/gkomissarov/Downloads/db'
 
 # Network
 #args=$args' --port=80'
-args=$args' --address=0.0.0.0'
+#args=$args' --address=0.0.0.0'
 
 # Update
 args=$args'  --skip_sdk_update_check'
@@ -22,16 +22,22 @@ if [ "$1" = "email" ]; then
 fi
 
 # common info
-clear
 UNAME=`uname`
 if [ $UNAME == "Darwin" ]
 then
     GAE_VER=`cat /Applications/GoogleAppEngineLauncher.app/Contents/Info.plist | grep -m1 "Google Inc" | awk '{print $2}'`
 fi
 
-python -V
-echo "GAE version: ""${GAE_VER%?}"
-echo 'passing args: '$args
+BRANCH=`git branch | head -1`
+PY_VER=`python -c "import platform; print platform.python_version()"`
+
+clear
+printf "%15s: %s\n%15s: %s\n%15s: %s\n%15s: %s\n"\
+    "python" "$PY_VER"\
+    "GAE" "${GAE_VER%?}"\
+    "branch" "$BRANCH"\
+    "passing args" "$args"
+
 for((i=1;i<=80;i++));do printf "%s" "+";done;printf "\n"
 
 # launch it
