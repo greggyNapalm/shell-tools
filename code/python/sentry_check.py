3a65fc27be5e5d8a5b94eec22cfb9d1129ccaeac
Gregory Komissarov <gregory.komissarov@gmail.com>
2015-04-08 17:15:22 +0300

    sentry example up to date

--- code/python/sentry_check.py
+++ code/python/sentry_check.py
@@ -4,7 +4,7 @@
 """
     app.module
     ~~~~~~~~~~~~~~~
-    
+
     Some info about this module.
 """
 import logging
@@ -12,21 +12,28 @@ from raven.handlers.logging import SentryHandler
 from raven.conf import setup_logging
 from raven import Client
 
-#DSN_EXAMPLE = '{PROTOCOL}://{PUBLIC_KEY}:{SECRET_KEY}@{HOST}/{PATH}{PROJECT_ID}'
+# DSN_EXAMPLE = '{PROTOCOL}://{PUBLIC_KEY}:{SECRET_KEY}@{HOST}/{PATH}{PROJECT_ID}'
+# use Youre own.
 
 def crash():
     c = never_exists
 
+
 def main():
     logger = logging.getLogger(__name__)
-    client = Client(dsn=DSN)
+    adapter = logging.LoggerAdapter(logger, {
+        'job': 'test_gelf_udp',
+        'worker_version': '0.1.2',
+    })
+
+    client = Client(DSN)
     handler = SentryHandler(client)
     setup_logging(handler)
 
     try:
         crash()
-    except Exception: 
-        logger.error('Test script call, all fine.', exc_info=True)
+    except Exception:
+        adapter.error('Test script call, all fine.', exc_info=True)
     print 'Done'
 
 if __name__ == '__main__':
